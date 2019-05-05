6e2b36492fb1302c8b950b2415029cf96c49f717
Brandon Chen <bchen00@stuy.edu>
2019-05-04 22:03:52 -0400

    able to get it past a 20 something by a 20 something board where it got stuck before

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,8 @@
+
+
+
+/////////////////////////////////////////////////////////////////
+/**
 import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
@@ -140,7 +145,7 @@ public class KnightBoard{
 		if (startingRow < 0 || startingCol < 0 || startingRow > board.length-1 || startingCol > board[startingRow].length - 1){
 			throw new IllegalArgumentException("Out of bounds");
 		}
-		return solveH(startingRow,startingCol, 1);
+		return solveHelper(startingRow,startingCol, 1);
 	}
 
 	public void fill(int row, int col){
@@ -219,7 +224,7 @@ public class KnightBoard{
 	}
 
 
-private boolean solveH(int row, int col, int level){
+private boolean solveHelper(int row, int col, int level){
 	//possibleMo
 	if (level == board.length * board[0].length){
 		return true;
@@ -233,7 +238,7 @@ private boolean solveH(int row, int col, int level){
 			//System.out.println("YES");
 			addKnight(row,col,level);
 			fill(row,col);
-			if (solveH(coord[0][i],coord[1][i],level+1)){
+			if (solveHelper(coord[0][i],coord[1][i],level+1)){
 				return true;
 			}
 			removeKnight(row,col,level);
@@ -271,8 +276,7 @@ private int numberOfMoves(int row, int col){
 	return ans;
 }
 /**
-	private boolean solveH(int row, int col, int level){
-
+	private boolean solveHelper(int row, int col, int level){
 		if (board[row][col] == 0){
 			board[row][col] = level;
 		}
@@ -281,58 +285,57 @@ private int numberOfMoves(int row, int col){
 		}
 		// checks for movement
 		if (row > 1 && col > 0 && board[row-2][col-1] == 0){ // up 2 left 1
-			if (solveH(row-2,col-1,level + 1)){
+			if (solveHelper(row-2,col-1,level + 1)){
 				return true;
 			}
 			board[row-2][col-1] = 0;
 		}
 		if (row > 1 && col < board[row].length - 1 && board[row-2][col+1] == 0){  // up 2 right 1
-			if (solveH(row-2,col+1,level+1)){
+			if (solveHelper(row-2,col+1,level+1)){
 				return true;
 			}
 			board[row-2][col+1] = 0;
 		}
 		if (row > 0 && col > 1 && board[row-1][col-2] == 0){ // up 1 left 2
-			if (solveH(row-1,col-2,level+1)){
+			if (solveHelper(row-1,col-2,level+1)){
 				return true;
 			}
 			board[row-1][col-2] = 0;
 		}
 		if (row > 0 && col < board[row].length - 2 && board[row-1][col+2] == 0){ //up 1 right 2
-			if (solveH(row-1,col+2,level+1)){
+			if (solveHelper(row-1,col+2,level+1)){
 				return true;
 			}
 			board[row-1][col+2] = 0;
 		}
 		if (row < board.length - 1 && col > 1 && board[row+1][col-2] == 0){ // down 1 left 2
-			if (solveH(row + 1, col - 2,level+1)){
+			if (solveHelper(row + 1, col - 2,level+1)){
 				return true;
 			}
 			board[row+1][col-2] = 0;
 		}
 		if (row < board.length - 1 && col < board[row].length -2 && board[row + 1][col+2] == 0){ // down 1 right 2
-			if (solveH(row + 1, col + 2,level+1)){
+			if (solveHelper(row + 1, col + 2,level+1)){
 				return true;
 			}
 			board[row+1][col+2] = 0;
 		}
 		if (row < board.length - 2 && col > 0 && board[row + 2][col-1] == 0){ // down 2 left 1
-			if (solveH(row + 2, col -1, level+1)){
+			if (solveHelper(row + 2, col -1, level+1)){
 				return true;
 			}
 			board[row+2][col-1] = 0;
 		}
 		if (row < board.length - 2 && col < board[row].length - 1 && board[row+2][col+1] == 0){ // down 2 right 1
-			if (solveH(row + 2, col + 1,level +1)){
+			if (solveHelper(row + 2, col + 1,level +1)){
 				return true;
 			}
 			board[row+2][col+1] = 0;
 		}
 		return false;
-
 	}
-
 **/
+/**
 	private boolean solved(){
 		boolean ans = true;
 		for (int r = 0; r < board.length; r++){
@@ -457,7 +460,6 @@ public int countSolutionsHelper(int row, int col, int level, int ans){
 		ans += 1;
 		return ans;
 	}
-
 	//recursion
 	if (row > 1 && col > 0 && board[row-2][col-1] == 0){ // up 2 left 1
 		if (countSolutionsHelper(row-2,col-1,level + 1,ans) > 0){
@@ -509,10 +511,7 @@ public int countSolutionsHelper(int row, int col, int level, int ans){
 	}
 	board[row][col] = 0;
 	return ans;
-
 }
-
-
 /**
  	public int countSolutionsHelper(int row, int col, int level,int ans){
 		if (solved()){
@@ -522,7 +521,6 @@ public int countSolutionsHelper(int row, int col, int level, int ans){
 		//System.out.println("" + ans);
 		if (board[row][col] == 0){
 			board[row][col] = level;
-
 		// checks for movement
 		if (row > 1 && col > 0 && board[row-2][col-1] == 0){ // up 2 left 1
 			//if (countSolutionsHelper(row-2,col-1,level + 1,ans) > 0){
@@ -579,5 +577,195 @@ public int countSolutionsHelper(int row, int col, int level, int ans){
 	**/
 
 
+
+//}
+
+
+import java.util.Arrays;
+
+public class KnightBoard{
+	private int[][] board;
+	public int[][] board2;
+	public int[][] move = {{1,-2},{2,1},{2,-1},{-1,2},{-1,-2},{-2,1},{-2,-1},{1,2}};
+	public int count;
+	public KnightBoard(int rows,int cols) {
+		count = 0;
+		board = new int[rows][cols];
+		board2 = new int[rows][cols];
+		fills();
+	}
+
+	public void fills() {
+		if(board.length > 0) {
+			board2 = new int[board.length][board[0].length];
+		}
+		for(int r = 0; r < board2.length; r++) {
+			for(int c = 0; c < board2[r].length; c++) {
+				for(int n = 0; n < move.length; n++) {
+					if(r+move[n][0] >= 0 && r+move[n][0] < board2.length && c+move[n][1] >= 0 &&
+					c+move[n][1] < board2[0].length && board[r+move[n][0]][c+move[n][1]] == 0){
+						board2[r][c]++;
+					}
+				}
+			}
+		}
+	}
+
+	public boolean solve(int startRow, int startCol) {
+		if(startRow < 0 || startCol < 0) {
+			throw new IllegalArgumentException();
+		}
+		for(int r = 0; r < board2.length; r++) {
+			for(int c = 0; c < board2[r].length; c++) {
+				if(board[r][c] != 0) {
+					throw new IllegalStateException();
+				}
+			}
+		}
+		if(board.length == 0) {
+			return false;
+		}
+		return solveHelper(startRow,startCol,1);
+	}
+
+	private boolean solveHelper(int r, int c, int pos) {
+		if(pos == board.length * board[0].length) {
+			board[r][c] = pos;
+			return true;
+		}
+		int[][] moves = getMoves(r,c);
+		board[r][c] = pos;
+		for(int i = 0; i < moves.length; i++) {
+			if(solveHelper(moves[i][1],moves[i][2],pos+1)) {
+				return true;
+			}
+		}
+		board[r][c] = 0;
+		board = new int[board.length][board[0].length];
+		return false;
+	}
+
+	public int countSolutions(int startRow, int startCol) {
+		if(startRow < 0 || startCol < 0) {
+			throw new IllegalArgumentException();
+		}
+		for(int r = 0; r < board2.length; r++) {
+			for(int c = 0; c < board2[r].length; c++) {
+				if(board[r][c] != 0) {
+					throw new IllegalStateException();
+				}
+			}
+		}
+		if(board.length == 0) {
+			return 0;
+		}
+		count = 0;
+		countSolutionHelper(startRow,startCol,1);
+		return count;
+	}
+
+	private void countSolutionHelper(int r, int c, int pos) {
+		if(pos == board.length * board[0].length) {
+			count++;
+		}
+		int[][] moves = getMoves(r,c);
+		board[r][c] = pos;
+		for(int i = 0; i < moves.length; i++) {
+			countSolutionHelper(moves[i][1],moves[i][2],pos+1);
+		}
+		board[r][c] = 0;
+	}
+
+	public int[][] getMoves(int r, int c) {
+		fills();
+		int[][] moves = new int[board2[r][c]][3];
+		int i = 0;
+		if(board2[r][c] > 0) {
+			for(int n = 0; n < move.length; n++) {
+				if(r+move[n][0] >= 0 && r+move[n][0] < board2.length && c+move[n][1] >= 0 && c+move[n][1] < board2[0].length && board[r+move[n][0]][c+move[n][1]] == 0) {
+					moves[i][0] = board2[r+move[n][0]][c+move[n][1]];
+					moves[i][1] = r+move[n][0];
+					moves[i][2] = c+move[n][1];
+					i++;
+				}
+			}
+			return sort(moves);
+		}
+		return moves;
+	}
+
+
+
+
+	private int[][] sort(int[][] ary) {
+		if(ary.length == 1) {
+			return ary;
+		}
+		else {
+			int n = 0;
+			int m = ary.length/2;
+			int[][] temp1 = new int[m][3];
+			int[][] temp2 = new int[ary.length-m][3];
+			for(int i = 0; i < temp1.length; i++) {
+				temp1[i] = ary[n];
+				n++;
+			}
+			for(int i = 0; i < temp2.length; i++) {
+				temp2[i] = ary[n];
+				n++;
+			}
+			return merge(sort(temp1),sort(temp2));
+		}
+	}
+
+	private int[][] merge(int[][] ary1,int[][] ary2) {
+		int[][] out = new int[ary1.length+ary2.length][3];
+		int o = 0;
+		int t = 0;
+		int n = 0;
+		for(;n < out.length; n++) {
+			if(o == ary1.length) {
+				for(int i = n; i < out.length; i++) {
+					out[i] = ary2[t];
+					t++;
+				}
+				return out;
+			}
+			if(t == ary2.length) {
+				for(int i = n; i < out.length; i++) {
+					out[i] = ary1[o];
+					o++;
+				}
+				return out;
+			}
+			if(ary1[o][0] >= ary2[t][0]) {
+				out[n] = ary2[t];
+				t++;
+			}
+			else {
+				if(ary1[o][0] < ary2[t][0]) {
+					out[n] = ary1[o];
+					o++;
+				}
+			}
+		}
+		return out;
+	}
+
+	public String toString() {
+		String out = "";
+		for(int r = 0; r < board.length; r++) {
+			for(int c = 0; c < board[r].length; c++) {
+				if(board[r][c] < 10) {
+				out += " " + board[r][c] + " ";
+				}
+				else {
+					out += board[r][c] + " ";
+				}
+			}
+			out += "\n";
+		}
+		return out;
+	}
 
 }
